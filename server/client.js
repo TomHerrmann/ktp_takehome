fe1168095661633ac1635037a6dae1034a026e5c
Tom Herrmann <tomherrmanndev@gmail.com>
2020-06-17 17:17:11 -0400

    initial setup

---
+++ server/client.js
@@ -0,0 +1,18 @@
+const { Client } = require('pg');
+
+// create Heroku PostgresSQL db: https://elements.heroku.com/addons/heroku-postgresql
+const connectionString = 'PASTE YOUR CONNECTION STRING HERE';
+
+const client = new Client({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+client.connect((err) => {
+  if (err) console.log(err);
+  else console.log('Connected to database!');
+});
+
+module.exports = client;
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="title-container">
+        <h1>This is the start of something great!</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
