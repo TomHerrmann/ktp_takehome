790bc8a6abe6d8a4ece16295b33f46bfe51e150f
Tom Herrmann <tomherrmanndev@gmail.com>
2020-06-18 14:09:45 -0400

    Added actions

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,6 +1,26 @@
 import * as types from '../constants/actionTypes';
 
-// export const ____ = bookId => ({
-//   type: types._____,
-//   payload: _____
-// });
+export const api_error = (error) => ({
+  type: types.API_ERROR,
+  payload: error,
+});
+
+export const books_ad = () => ({
+  type: types.BOOKS_ADD,
+  payload: null,
+});
+
+export const books_populate = (books) => ({
+  type: types.BOOKS_POPULATE,
+  payload: books,
+});
+
+export const form_submit = (formInput) => ({
+  type: types.FORM_SUBMIT,
+  payload: formInput,
+});
+
+export const search_submit = (searchQuery) => ({
+  type: types.SEARCH_SUBMIT,
+  payload: searchQuery,
+});
