4bc3001b2aa1f35f5fb265635c9bcd240a229c4e
Tom Herrmann <tomherrmanndev@gmail.com>
2020-06-18 14:10:07 -0400

    Updated book reducer

--- src/reducers/bookReducer.js
+++ src/reducers/bookReducer.js
@@ -4,6 +4,17 @@ const initialState = {};
 
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
+    case types.API_ERROR:
+      return state;
+    case types.BOOKS_ADD:
+      return state;
+    case types.BOOKS_POPULATE:
+      console.log('books in reducer --> ', books);
+      return Object.assign({}, state, { books });
+    case types.FORM_SUBMIT:
+      return state;
+    case types.SEARCH_SUBMIT:
+      return state;
     default:
       return state;
   }
