2f395e15818a83bc57cb9cf0674af5be32da617b
Tom Herrmann <tomherrmanndev@gmail.com>
2020-06-18 14:09:53 -0400

    Added action types

---
+++ src/constants/actionTypes.js
@@ -0,0 +1,5 @@
+export const API_ERROR = 'API_ERROR';
+export const BOOKS_ADD = 'BOOKS_ADD';
+export const BOOKS_POPULATE = 'BOOKS_POPULATE';
+export const FORM_SUBMIT = 'FORM_SUBMIT';
+export const SEARCH_SUBMIT = 'SEARCH_SUBMIT';
