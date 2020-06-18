9eee487cb5d023e19c228cf0d648adf7f42604ea
Tom Herrmann <tomherrmanndev@gmail.com>
2020-06-18 18:51:56 -0400

    Updated card layout

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 
 const BookCards = ({ book }) => {
-  const { title, author, publisher } = book;
+  const { title, author, publisher, publishDate } = book;
 
   return (
     <article className="book-card">
+      <h2>Title: {title}</h2>
       <ul>
-        <li>
-          <strong>Title: </strong>
-          {title}
-        </li>
         <li>
           <strong>Author: </strong>
           {author}
         </li>
         <li>
           <strong>Publisher: </strong>
+          {publisher}
+        </li>
+        <li>
+          <strong>Publish Date: </strong>
           {publisher}
         </li>
       </ul>
