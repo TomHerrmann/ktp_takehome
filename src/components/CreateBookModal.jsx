166f2eb5307075633776e278e62125999dbdb4e3
Tom Herrmann <tomherrmanndev@gmail.com>
2020-06-18 20:34:21 -0400

    Create modal form

--- src/components/CreateBookModal.jsx
+++ src/components/CreateBookModal.jsx
@@ -3,8 +3,47 @@ import ReactModal from 'react-modal';
 
 const CreateBookModal = () => {
   return (
-    <ReactModal isOpen={true}>
+    <ReactModal isOpen={true} className="create-book-modal">
       <h3>Create New Book</h3>
+      <form>
+        <label>
+          Title
+          <input
+            type="text"
+            placeholder="Title"
+            onChange={() => {}}
+            // value={modalTitle}
+          />
+        </label>
+        <label>
+          Author
+          <input
+            type="text"
+            placeholder="Authors"
+            onChange={() => {}}
+            // value={modalTitle}
+          />
+        </label>
+        <label>
+          Publisher
+          <input
+            type="text"
+            placeholder="Publisher"
+            onChange={() => {}}
+            // value={modalTitle}
+          />
+        </label>
+        <label>
+          Published Date
+          <input
+            type="text"
+            placeholder="Published Date"
+            onChange={() => {}}
+            // value={modalTitle}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
     </ReactModal>
   );
 };
